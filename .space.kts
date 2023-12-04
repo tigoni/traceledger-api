47e1e53e69051bb05e83a0ac8a1433ee78e76499
Kelly Muhindi <muhindi.kn@gmail.com>
2023-12-04 12:11:50 +0000

    Add deploy to `staging` script

--- .space.kts
+++ .space.kts
@@ -13,4 +13,16 @@ job("Build and Run Tests") {
             """
         }
     }
+
+    container("amazoncorretto:17-alpine") {
+    kotlinScript { api ->
+        api.space().projects.automation.deployments.start(
+            project = api.projectIdentifier(),
+            targetIdentifier = TargetIdentifier.Key("staging"),
+            version = "1.0.0",
+            // automatically update deployment status based on a status of a job
+            syncWithAutomationJob = true
+        )
+    }
+}
 }
